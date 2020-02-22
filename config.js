1a56fe6fcdb35338ec1823690e8fc6b4db1433dc
houssam <houssamrakib21@gmail.com>
2020-02-22 11:43:04 +0100

    env

---
+++ config.js
@@ -0,0 +1,4 @@
+export default {
+    WS_URI: 'ws://localhost:4000/ws',
+    HTTP_URI: 'http://localhost:4000/'
+}
--- src/config.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config()
-
-export default {
-    WS_URI: process.env.WS_URI,
-    HTTP_URI: process.env.HTTP_URI
-}
