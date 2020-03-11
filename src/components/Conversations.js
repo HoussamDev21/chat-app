6983e0a9a97136d7e6dd864f9635ddbe7a5747d9
houssam <houssamrakib21@gmail.com>
2020-03-11 17:45:40 +0100

    real time conversations

--- src/components/Conversations.js
+++ src/components/Conversations.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ConversationItem from './ConversationItem'
-import { useQuery } from 'react-apollo'
+import { useQuery, useSubscription } from 'react-apollo'
 import gql from 'graphql-tag'
+import _ from 'lodash'
 
 const CONVERSATIONS = gql`{
     conversations {
@@ -20,8 +21,46 @@ const CONVERSATIONS = gql`{
     }
 }`
 
+const CONVERSATION_SUBSCRIPTION = gql`subscription {
+    conversation {
+        id
+        created_at
+        participants {
+            id
+            username
+        },
+        lastMessage {
+            id
+            content
+            created_at
+            user_id
+        }
+    }
+}`
+
 export default function Conversations () {
-    let { data, loading, error } = useQuery(CONVERSATIONS)
+    const [conversations, setConversations] = useState([])
+    const { loading, error } = useQuery(CONVERSATIONS, {
+        variables: {},
+        onCompleted: (data) => {
+            setConversations(data.conversations)
+        }
+    })
+
+    useSubscription(CONVERSATION_SUBSCRIPTION, { 
+        variables: {},
+        onSubscriptionData: ({ subscriptionData: { data }}) => {
+            let list = [...conversations]
+            let index = list.findIndex(c => c.id === data.conversation.id)
+            if (index > -1) {
+                list[index] = data.conversation
+            } else {
+                list.unshift(data.conversation)
+            }
+            list = _.orderBy(list, 'lastMessage.created_at', 'desc')
+            setConversations(list)
+        }
+    })
 
     if (loading) {
         return <div className="p-3 text-center text-gray-500">Loading ...</div>
@@ -31,9 +70,9 @@ export default function Conversations () {
         return 'error'
     }
 
-    if (!data.conversations.length) {
+    if (!conversations.length) {
         return <div className="p-3 text-center text-gray-500">No conversations yet !</div>
     }
 
-    return data.conversations.map((c) => <ConversationItem key={c.id} item={c} />)
+    return conversations.map((c) => <ConversationItem key={c.id} item={c} />)
 }
